501bd137482d13bb44b72038c7005065a8a77e19
erisson.batista@ask.com <erissonbreno@hotmail.com>
2023-10-02 09:31:25 -0300

    Adiciona Controllers

---
+++ src/main/java/br/edu/infnet/aluguelVeiculo/controller/CarroController.java
@@ -0,0 +1,23 @@
+//package br.edu.infnet.aluguelVeiculo.controller;
+//
+//import br.edu.infnet.aluguelVeiculo.ClienteLoader;
+//import lombok.Setter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//@Setter
+//public class CarroController {
+//
+//    @Autowired
+//    private ClienteLoader clienteLoader;
+//
+//    @GetMapping(value = "/cliente/lista")
+//    public String telaCliente(Model model) {
+//
+////        model.addAttribute("listaCliente", clienteLoader.obterLista());
+//        return "cliente/lista";
+//    }
+//}
